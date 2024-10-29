aaac7c83c64001a85bef32dff9724fe0d79525e8
Guilhermecmachado <guicamchado@gmail.com>
2024-10-29 14:39:04 -0300

    Initial commit

---
+++ index.test.js
@@ -0,0 +1,9 @@
+// index.test.js
+const request = require('supertest');
+const express = require('express');
+const app = require('./index'); // Se precisar exportar o app
+
+test('deve responder com Hello World!', async () => {
+    const response = await request(app).get('/');
+    expect(response.text).toBe('Hello World!');
+});
